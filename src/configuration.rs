729532a4da8ccc2b504e138db813169519851f45
Omar Alhariri <omar_alhariri@outlook.com>
2023-04-14 04:07:03 +0300

    Chapter 3 part 1

---
+++ src/configuration.rs
@@ -0,0 +1,42 @@
+use config::Config;
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    //let mut settings = config::Config::default();
+
+    // Add configuration values from a file named `configuration`.
+    // It will look for any top-level file with an extension
+    // that `config` knows how to parse: yaml, json, etc.
+    let mut settings = Config::builder()
+        .set_default("default", "1")?
+        .add_source(config::File::with_name("configuration.yaml"))
+        .build();
+
+    match settings {
+        Ok(config) => config.try_deserialize::<Settings>(),
+        Err(e) => Err(e),
+    }
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
