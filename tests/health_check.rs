bbcbf2dae8aa0d6cabfa3050f04f22b6da42b054
Omar Alhariri <omar_alhariri@outlook.com>
2023-04-14 04:07:03 +0300

    Chapter 3 part 1

---
+++ tests/health_check.rs
@@ -0,0 +1,102 @@
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+
+use std::net::TcpListener;
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::{get_configuration, self};
+
+// No .await call, therefore no need for `spawn_app` to be async now.
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can just panic and crash
+// all the things.
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+
+    format!("http:127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let body = "name=omar%20alhariri&email=o.alhariri%40outlook.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to excute request");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "o.alhariri@outlook.com");
+    assert_eq!(saved.name, "omar alhariri");
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_invalid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to excute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The api did not fail when the payload was {}",
+            error_message
+        );
+    }
+}
