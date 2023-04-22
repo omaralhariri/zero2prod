ced686057233bbc938aa88e7d87f01bb5e484497
Omar Alhariri <omar_alhariri@outlook.com>
2023-04-23 00:18:13 +0300

    Validate login with query params

--- src/startup.rs
+++ src/startup.rs
@@ -5,7 +5,7 @@ use crate::routes::{confirm, health_check, login, login_form, publish_newsletter
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
-use secrecy::ExposeSecret;
+use secrecy::{ExposeSecret, Secret};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -44,6 +44,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            HmacSecret(configuration.application.hmac_secret),
         )?;
 
         Ok(Self { port, server })
@@ -65,6 +66,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: HmacSecret,
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
@@ -83,6 +85,7 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(hmac_secret.clone()))
     })
     .listen(listener)?
     .run();
@@ -96,3 +99,6 @@ pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
         .connect_lazy(configuration.connection_string().expose_secret())
         .unwrap()
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
