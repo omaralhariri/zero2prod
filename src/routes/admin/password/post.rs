b83a9ddc4d57cfa1ac1684e76e0ab69d53e8964e
Omar Alhariri <omar_alhariri@outlook.com>
2023-04-28 17:39:59 +0300

    Refactoring and using middleware instead of a function

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,13 +1,11 @@
-use actix_web::{error::InternalError, web, HttpResponse};
+use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
-use uuid::Uuid;
 
 use crate::{
-    authentication::{validate_credentials, Credentials},
+    authentication::{validate_credentials, Credentials, UserId},
     routes::admin::dashboard::get_username,
-    session_state::TypedSession,
     utils::{e500, see_other},
 };
 
@@ -20,10 +18,10 @@ pub struct FormData {
 
 pub async fn change_password(
     form: web::Form<FormData>,
-    session: TypedSession,
     pool: web::Data<PgPool>,
+    user_id: web::ReqData<UserId>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let user_id = reject_anonymous_users(session).await?;
+    let user_id = user_id.into_inner();
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
             "You entered two different new passwords - the field values must match",
@@ -33,7 +31,7 @@ pub async fn change_password(
         return Ok(see_other("/admin/password"));
     }
 
-    let username = get_username(user_id, &pool).await.map_err(e500)?;
+    let username = get_username(*user_id, &pool).await.map_err(e500)?;
     let credentials = Credentials {
         username,
         password: form.0.current_password,
@@ -45,7 +43,7 @@ pub async fn change_password(
                 FlashMessage::error("The current password is incorrect").send();
                 Ok(see_other("/admin/password"))
             }
-            crate::authentication::AuthError::UnexpectedError(_) => Err(e500(e).into()),
+            crate::authentication::AuthError::UnexpectedError(_) => Err(e500(e)),
         };
     }
 
@@ -60,22 +58,11 @@ pub async fn change_password(
         return Ok(see_other("/admin/password"));
     }
 
-    crate::authentication::change_password(user_id, form.0.new_password, &pool)
+    crate::authentication::change_password(*user_id, form.0.new_password, &pool)
         .await
         .map_err(e500)?;
 
     FlashMessage::info("Your password has been changed").send();
 
     Ok(see_other("/admin/password"))
-}
-
-async fn reject_anonymous_users(session: TypedSession) -> Result<Uuid, actix_web::Error> {
-    match session.get_user_id().map_err(e500)? {
-        Some(user_id) => Ok(user_id),
-        None => {
-            let response = see_other("/login");
-            let e = anyhow::anyhow!("The user has not logged in");
-            Err(InternalError::from_response(e, response).into())
-        }
-    }
 }
